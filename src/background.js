d8d97c158065e8904aff0107d207d5a838d9dfe3
GinirohikoCha <rinkouatf@qq.com>
2021-04-03 14:54:37 +0800

    添加依赖

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, ipcMain } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
@@ -16,7 +16,7 @@ async function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-
+      webSecurity: false,
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
