6f36df99d7634c7c004cb620b0c1dc6c9ef58c6b
GinirohikoCha <rinkouatf@qq.com>
2021-04-18 20:45:28 +0800

    添加依赖vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+
 import App from './App.vue'
+
+const routes = [
+  { path: '/', component: () => import('@/view/Viewer') },
+  { path: '/config', component: () => import('@/view/Config') }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
