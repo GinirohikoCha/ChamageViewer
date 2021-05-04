2f57810fbaa373dde57505cf554071247e22f8f5
GinirohikoCha <rinkouatf@qq.com>
2021-05-04 20:38:02 +0800

    模块化重构

--- src/background.js
+++ src/background.js
@@ -1,10 +1,9 @@
 'use strict'
 
-import { app, protocol, ipcMain, BrowserWindow, Menu, dialog } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import { app, protocol, BrowserWindow } from 'electron'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-const configUtil = require('@/util/config')
-const imageUtil = require('@/util/image')
+
+const window = require('@/electron/window')
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -12,72 +11,7 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-let win = null
-let winSetting = null
 console.debug(process.env.WEBPACK_DEV_SERVER_URL)
-
-async function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      webSecurity: false,
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    },
-    show: false
-  })
-  Menu.setApplicationMenu(null)
-
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
-
-  win.once('ready-to-show', () => {
-    win.show()
-  })
-}
-
-function createSettingWindow () {
-  if (winSetting) {
-    winSetting.show()
-  } else {
-    winSetting = new BrowserWindow({
-      width: 600,
-      height: 400,
-      parent: win,
-      resizable: false,
-      webPreferences: {
-        webSecurity: false,
-        nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-      },
-      show: false
-    })
-
-    if (process.env.WEBPACK_DEV_SERVER_URL) {
-      winSetting.loadURL(process.env.WEBPACK_DEV_SERVER_URL + '#/config')
-      if (!process.env.IS_TEST) winSetting.webContents.openDevTools()
-    } else {
-      winSetting.loadURL('app://./index.html/#/config')
-    }
-    winSetting.once('ready-to-show', () => {
-      winSetting.setTitle('设置')
-      winSetting.show()
-    })
-    winSetting.on('close', (e) => {
-      e.preventDefault()
-      winSetting.hide()
-    })
-  }
-}
 
 let processUrl = ''
 if (process.env.WEBPACK_DEV_SERVER_URL) {
@@ -99,7 +33,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) window.openMainWindow()
 })
 
 // This method will be called when Electron has finished
@@ -121,57 +55,7 @@ app.on('ready', async () => {
     }
   }
   // 创建窗口
-  createWindow()
-  // 进程通信
-  // 配置操作
-  ipcMain.on('open-config', function (event) {
-    createSettingWindow()
-  })
-  ipcMain.on('load-config', function (event) {
-    const config = configUtil.loadConfig()
-    console.log('已加载配置:')
-    console.log(config)
-    event.sender.send('config-loaded', config)
-  })
-  ipcMain.on('update-config', function (event, newConfig) {
-    configUtil.updateConfig(newConfig)
-    win.webContents.send('config-loaded', newConfig)
-  })
-  ipcMain.on('get-config', function (event) {
-    event.returnValue = configUtil.getConfig()
-  })
-  // 图片操作
-  ipcMain.on('open-image', function (event) {
-    processUrl = dialog.showOpenDialogSync(win, {
-      title: '打开图片',
-      filters: [
-        { name: 'Images', extensions: ['bmp', 'jpg', 'jpeg', 'png', 'gif', 'tif', 'svg', 'psd', 'ai', 'raw', 'webp'] }
-      ],
-      properties: ['openFile']
-    })[0].replaceAll('\\', '/')
-    event.returnValue = imageUtil.openImage(processUrl)
-    win.setTitle(imageUtil.getImageTitle())
-  })
-  ipcMain.on('init-image', function (event) {
-    event.returnValue = imageUtil.openImage(processUrl)
-    win.setTitle(imageUtil.getImageTitle())
-  })
-  ipcMain.on('delete-image', function (event) {
-    event.returnValue = imageUtil.deleteImage()
-    win.setTitle(imageUtil.getImageTitle())
-  })
-  ipcMain.on('next-image', function (event) {
-    event.returnValue = imageUtil.getNxtImage()
-    win.setTitle(imageUtil.getImageTitle())
-  })
-  ipcMain.on('pre-image', function (event) {
-    event.returnValue = imageUtil.getPreImage()
-    win.setTitle(imageUtil.getImageTitle())
-  })
-  ipcMain.on('test', function (event) {
-    const exePath = process.argv[0].replaceAll('\\', '/')
-    event.returnValue = exePath.substring(0, exePath.lastIndexOf('/'))
-  })
+  window.initWindow(processUrl)
 })
 
 // Exit cleanly on request from parent process in development mode.
---
+++ src/electron/window.js
@@ -0,0 +1,133 @@
+import { BrowserWindow, dialog, ipcMain, Menu } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+
+const configUtil = require('@/util/config')
+const fileUtil = require('@/util/file')
+
+export let mainWindow = null
+export let settingWindow = null
+
+export function initWindow (processUrl) {
+  openMainWindow()
+  // 进程通信
+  // 配置操作
+  ipcMain.on('open-config', function (event) {
+    openSettingWindow()
+  })
+  ipcMain.on('load-config', function (event) {
+    const config = configUtil.loadConfig()
+    console.log('已加载配置:')
+    console.log(config)
+    event.sender.send('config-loaded', config)
+  })
+  ipcMain.on('update-config', function (event, newConfig) {
+    configUtil.updateConfig(newConfig)
+    mainWindow.webContents.send('config-loaded', newConfig)
+  })
+  ipcMain.on('get-config', function (event) {
+    event.returnValue = configUtil.getConfig()
+  })
+  // 图片操作
+  ipcMain.on('open-image', function (event) {
+    processUrl = dialog.showOpenDialogSync(mainWindow, {
+      title: '打开图片',
+      filters: [
+        { name: 'Images', extensions: ['bmp', 'jpg', 'jpeg', 'png', 'gif', 'tif', 'svg', 'psd', 'ai', 'raw', 'webp'] }
+      ],
+      properties: ['openFile']
+    })[0].replaceAll('\\', '/')
+    event.returnValue = fileUtil.openImage(processUrl)
+    mainWindow.setTitle(fileUtil.getImageTitle())
+  })
+  ipcMain.on('init-image', function (event) {
+    event.returnValue = fileUtil.openImage(processUrl)
+    mainWindow.setTitle(fileUtil.getImageTitle())
+  })
+  ipcMain.on('delete-image', function (event) {
+    event.returnValue = fileUtil.deleteImage()
+    mainWindow.setTitle(fileUtil.getImageTitle())
+  })
+  ipcMain.on('next-image', function (event) {
+    event.returnValue = fileUtil.getNxtImage()
+    mainWindow.setTitle(fileUtil.getImageTitle())
+  })
+  ipcMain.on('pre-image', function (event) {
+    event.returnValue = fileUtil.getPreImage()
+    mainWindow.setTitle(fileUtil.getImageTitle())
+  })
+  ipcMain.on('test', function (event) {
+    const exePath = process.argv[0].replaceAll('\\', '/')
+    event.returnValue = exePath.substring(0, exePath.lastIndexOf('/'))
+  })
+}
+
+export function openMainWindow () {
+  createMainWindow()
+}
+
+export function openSettingWindow () {
+  if (settingWindow) {
+    settingWindow.show()
+  } else {
+    createSettingWindow()
+  }
+}
+
+async function createMainWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false,
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+    },
+    show: false
+  })
+  Menu.setApplicationMenu(null)
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) mainWindow.webContents.openDevTools()
+  } else {
+    createProtocol('app')
+    // Load the index.html when not in development
+    mainWindow.loadURL('app://./index.html')
+  }
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+}
+
+function createSettingWindow () {
+  settingWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    parent: mainWindow,
+    resizable: false,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+    },
+    show: false
+  })
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    settingWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL + '#/config')
+    if (!process.env.IS_TEST) settingWindow.webContents.openDevTools()
+  } else {
+    settingWindow.loadURL('app://./index.html/#/config')
+  }
+  settingWindow.once('ready-to-show', () => {
+    settingWindow.setTitle('设置')
+    settingWindow.show()
+  })
+  settingWindow.on('close', (e) => {
+    e.preventDefault()
+    settingWindow.hide()
+  })
+}
